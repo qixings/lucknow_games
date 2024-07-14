d26aea67c16e68129e342ce30c1b49ee906430df
qixing <80924366+qixings@users.noreply.github.com>
2024-07-14 09:59:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
 document.querySelector('.close-menu').addEventListener('click', function() {
     document.querySelector('.hamburger-menu-overlay').style.display = 'none';
 });
-s
+
+
+// Display the popup when the page loads
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('popup').style.display = 'block';
+    }, 1000); // Adjust the timeout as needed (1000ms = 1s)
+};
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
